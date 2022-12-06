25c4fe609f8bf982415487ddc2f314cb5ae69563
Om Budhiraja <ombudhiraja144@gmail.com>
2022-12-06 16:44:44 +0530

    Day 6

---
+++ 6/b.ts
@@ -0,0 +1,24 @@
+// const input = await Deno.readTextFile('./test.txt');
+const input = await Deno.readTextFile('./input.txt');
+
+const processed = input
+  .replace(/\r/g, '') // remove all \r characters
+  .trim()
+  .split('');
+
+const packet: string[] = [];
+
+for (let i = 0; i < processed.length; i++) {
+  const char = processed[i];
+  if (!packet.includes(char)) {
+    packet.push(char);
+  } else {
+    packet.splice(0, packet.indexOf(char) + 1);
+    packet.push(char);
+  }
+
+  if (packet.length === 14) {
+    console.log(i + 1);
+    break;
+  }
+}
